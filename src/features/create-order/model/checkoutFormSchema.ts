f6ce1d14ec80d61bfbb65f06ba009212c694b923
Ihor <iv4enkoigor@gmail.com>
2026-06-29 23:26:55 +0300

    feat: add zod validation schema for checkout form fields

--- src/features/create-order/model/checkoutFormSchema.ts
+++ src/features/create-order/model/checkoutFormSchema.ts
@@ -1,8 +1,14 @@
 import { z } from "zod";
 
 export const checkoutFormSchema = z.object({
-  firstName: z.string().min(1, { message: "First name is required" }),
-  lastName: z.string().min(1, { message: "Last name is required" }),
+  firstName: z
+    .string()
+    .min(1, { message: "First name is required" })
+    .max(50, { message: "First name cannot be longer than 50 characters" }),
+  lastName: z
+    .string()
+    .min(1, { message: "Last name is required" })
+    .max(50, { message: "Last name cannot be longer than 50 characters" }),
   phone: z.string().min(1, { message: "Phone is required" }),
   email: z
     .string()
